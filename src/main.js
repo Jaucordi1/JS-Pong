c546edcf55997abbd342450723bd82b98c1be7d7
Jaucordi <jaucordi@gmail.com>
2018-09-18 00:50:44 +0200

    - Adding 'Vec2' class to manage points in space.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 const WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
 
+class Vec2 {
+    /**
+     * Create a new Vector
+     *
+     * @param {number} x X coordinate
+     * @param {number} y Y coordinate
+     */
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Set coordinates to "x" and "y"
+     *
+     * @param {number} x New X coordinate
+     * @param {number} y New Y coordinate
+     */
+    set(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 class Pong {
     constructor(canvas) {
         this.canvas = canvas;
