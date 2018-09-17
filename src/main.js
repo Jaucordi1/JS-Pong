ba8f7e16268a8474e65723687ff552ae1fca6539
Jaucordi <jaucordi@gmail.com>
2018-09-18 00:58:02 +0200

    - Adding 'Box' class to manage easily hitboxes in space.

--- src/main.js
+++ src/main.js
@@ -23,6 +23,87 @@ class Vec2 {
         this.y = y;
     }
 }
+
+class Box {
+    /**
+     * Create a new box in space
+     *
+     * @param {Vec2} pos
+     * @param {Vec2} size
+     */
+    constructor(pos, size) {
+        this.pos = pos;
+        this.size = size;
+    }
+
+    get width() {
+        return this.size.x;
+    }
+
+    set width(w) {
+        this.size.x = w;
+    }
+
+    get height() {
+        return this.size.y;
+    }
+
+    set height(h) {
+        this.size.y = h;
+    }
+
+    get left() {
+        return this.pos.x;
+    }
+
+    set left(x) {
+        this.pos.x = x;
+    }
+
+    get top() {
+        return this.pos.y;
+    }
+
+    set top(y) {
+        this.pos.y = y;
+    }
+
+    get right() {
+        return this.pos.x + this.size.x - 1;
+    }
+
+    set right(x) {
+        this.pos.x = x - this.size.x;
+    }
+
+    get bottom() {
+        return this.pos.y + this.size.y - 1;
+    }
+
+    set bottom(y) {
+        this.pos.y = y - this.size.y;
+    }
+
+    get centerX() {
+        return this.pos.x + (this.size.x / 2);
+    }
+
+    set centerX(x) {
+        this.pos.x = x - (this.size.x / 2);
+    }
+
+    get centerY() {
+        return this.pos.y + (this.size.y / 2);
+    }
+
+    set centerY(y) {
+        this.pos.y = y - (this.size.y / 2);
+    }
+
+    get center() {
+        return new Vec2(this.centerX, this.centerY);
+    }
+}
 class Pong {
     constructor(canvas) {
         this.canvas = canvas;
