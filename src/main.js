ccc9e7ad6315d648499c7aeeef9d5b54d3f9677b
Jaucordi <jaucordi@gmail.com>
2018-09-18 00:46:20 +0200

    - Adding class 'Pong' and refactoring canvas drawing in it.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 const WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
 
+class Pong {
+    constructor(canvas) {
+        this.canvas = canvas;
+    }
+
+    get context() {
+        return this.canvas.getContext('2d');
+    }
+
+    draw() {
+        const ctx = this.context;
+
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    }
+}
+
 const canvas = document.getElementById('screen');
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
-const context = canvas.getContext('2d');
-
-context.fillStyle = 'black';
-context.fillRect(0, 0, WIDTH, HEIGHT);
+const PONG = new Pong(canvas);
+PONG.draw();
